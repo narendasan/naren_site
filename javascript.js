44dd05cdd5d84012c3d9eb6f31236b678b7db89f
narendassan <narendasam@me.com>
2012-10-22 21:27:32 -0600

    first commit

---
+++ javascript.js
@@ -0,0 +1,19 @@
+ 
+	
+function ajaxcallhome () {
+	$("#ajax").load('home.html').attr('Color');
+	
+}
+function ajaxcallhtml () {
+	$("#ajax").load('html.html').attr('color');
+	
+}	
+function ajaxcallcss () {
+	$("#ajax").load('css.html').attr('color');
+	
+}
+	
+function ajaxcallabout () {
+	$("#ajax").load('about.html').attr('color');
+	
+}
