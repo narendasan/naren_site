72ab867c27f8546ed1e2f5170d78d52d193ddea6
narendassan <narendasam@me.com>
2012-10-26 21:35:18 -0600

    finish projects

--- javascript.js
+++ javascript.js
@@ -20,3 +20,38 @@ function ajaxcallcool () {
 function ajaxcallcode () {
 	$("#ajax").load('code.html');
 }
+function ajaxcallnhd () {
+	$("#ajax").html('<object data="http://54844055.nhd.weebly.com/">');
+}
+function ajaxcallnoaa () {
+	$("#ajax").html('<object data="http://localhost/~naren/v4.0/test.html">');
+}
+/*function bottom () {
+	var x = Math.floor((Math.random()*10)+1)
+	if ( x == 1 ) {
+		var color = $(this).css('background-image:url('pattern1.jpg')');
+	} 
+	else if ( x == 2 ) {
+		var color = $(this).css('background-image:url('pattern2.jpg')');
+	} 
+	else if ( x == 3) {
+		var color = $(this).css('background-image:url('pattern3.jpg')');
+	} 
+	else if ( x == 4 ) {
+		var color = $(this).css('background-image:url('pattern4.jpg')');
+	} 
+	else if ( x == 5 ) {
+		var color = $(this).css('background-image:url('pattern5.jpg')');
+	} 
+	else if ( x == 6 ) {
+		var color = $(this).css('background-image:url('pattern6.jpg')');
+	} 
+	else if ( x == 7 ) {
+		var color = $(this).css('background-image:url('pattern7.jpg')');
+	} 
+	else {
+		var color = $(this).css('background-image:url('pattern3.jpg')');
+	} 
+	$("#lower").html( "<div id="lower" style='" +
+                     color + ";'>" "</div>")
+}*/
