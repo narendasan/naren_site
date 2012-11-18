caf259887718467d2c5b7eef219536c128661b18
narendassan <narendasam@me.com>
2012-11-18 16:13:01 -0700

    v2 and mobile

---
+++ v1/javascript.js
@@ -0,0 +1,28 @@
+ 
+	
+function ajaxcallhome () {
+	$("#ajax").load('about.html');
+	
+}
+function ajaxcallcontact () {
+	$("#ajax").load('contact.html');
+	
+}	
+function ajaxcallabout () {
+	$("#ajax").load('about.html');
+	
+}
+	
+function ajaxcallprojects () {
+	$("#ajax").load('cool.html');
+	
+}
+function ajaxcallcode () {
+	$("#ajax").load('code.html');
+}
+function ajaxcallnhd () {
+	$("#ajax").html('<object data="http://54844055.nhd.weebly.com/">');
+}
+function ajaxcallnoaa () {
+	$("#ajax").html('<object data="http://www.narendasan.com/noaa/test.html">');
+}
